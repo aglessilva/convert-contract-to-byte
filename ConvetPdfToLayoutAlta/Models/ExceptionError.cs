ce5b6e17450ba680499ac1e5a6282af140455028
Agles Silva <p000hsilva@prservicos.com.br>
2019-07-02 01:49:49 -0300

    comite de DEV

---
+++ ConvetPdfToLayoutAlta/Models/ExceptionError.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvetPdfToLayoutAlta.Models
+{
+    public static class ExceptionError
+    {
+        public static void TrataErros(Exception exception, string _contrato, string _detalhes, string _path)
+        {
+            using (StreamWriter sw = new StreamWriter(_path + @"\LogErroContratos.txt", true, Encoding.UTF8))
+            {
+                string _descricaoErro = exception == null ? "NÃO INFORMADO" : exception.Message;
+
+                StringBuilder strErro = new StringBuilder();
+                strErro.AppendLine(string.Format("CONTRATO: {0}",_contrato))
+                        .AppendLine(string.Format("DETALHES: {0}", _detalhes))
+                        .AppendLine(string.Format("DESCRIÇÃO DO ERRO: {0}", _descricaoErro));
+                sw.Write(strErro);
+                sw.WriteLine("================================================================================================================================================");
+            }
+
+        }
+
+        public static void NovoContratoGT(string _contrato, string _path)
+        {
+            using (StreamWriter sw = new StreamWriter(_path + @"\NovosContratos.txt", true, Encoding.UTF8))
+            {
+                if (_contrato != "0")
+                    sw.WriteLine(_contrato);
+            }
+
+        }
+    }
+}
