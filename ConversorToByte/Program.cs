b95be8b71bd1bfc2f012aebfaf977e568120d92b
Agles Silva <p000hsilva@prservicos.com.br>
2019-08-23 15:31:58 -0300

    [HAS] Versão 1.0 finalizada Data de conclusão: 2019-08-23

--- ConversorToByte/Program.cs
+++ ConversorToByte/Program.cs
@@ -16,7 +16,7 @@ namespace ConversorToByte
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmListContrato());
+            Application.Run(new FrmAddUsuariosAD());
         }
     }
 }
